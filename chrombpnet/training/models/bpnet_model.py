368a2120fd100f59bb7efae567e430fef65f29d2
lmawalker <80383011+lmawalker@users.noreply.github.com>
2024-02-05 13:16:49 -0800

    added same padding

--- chrombpnet/training/models/bpnet_model.py
+++ chrombpnet/training/models/bpnet_model.py
@@ -11,71 +11,71 @@ import os
 os.environ['PYTHONHASHSEED'] = '0'
 
 def getModelGivenModelOptionsAndWeightInits(args, model_params):
-    #default params (can be overwritten by providing model_params file as input to the training function)
-    conv1_kernel_size=21
-    profile_kernel_size=75
-    num_tasks=1 # not using multi tasking
-    
-    filters=int(model_params['filters'])
-    n_dil_layers=int(model_params['n_dil_layers'])
-    counts_loss_weight=float(model_params['counts_loss_weight'])
-    sequence_len=int(model_params["inputlen"])
-    out_pred_len=int(model_params["outputlen"])
+    # default params (can be overwritten by providing model_params file as input to the training function)
+    conv1_kernel_size = 21
+    profile_kernel_size = 75
+    num_tasks = 1  # not using multi tasking
+
+    filters = int(model_params['filters'])
+    n_dil_layers = int(model_params['n_dil_layers'])
+    counts_loss_weight = float(model_params['counts_loss_weight'])
+    sequence_len = int(model_params["inputlen"])
+    out_pred_len = int(model_params["outputlen"])
 
     print("params:")
-    print("filters:"+str(filters))
-    print("n_dil_layers:"+str(n_dil_layers))
-    print("conv1_kernel_size:"+str(conv1_kernel_size))
-    print("profile_kernel_size:"+str(profile_kernel_size))
-    print("counts_loss_weight:"+str(counts_loss_weight))
-    
-    #read in arguments
-    seed=args.seed
-    np.random.seed(seed)    
+    print("filters:" + str(filters))
+    print("n_dil_layers:" + str(n_dil_layers))
+    print("conv1_kernel_size:" + str(conv1_kernel_size))
+    print("profile_kernel_size:" + str(profile_kernel_size))
+    print("counts_loss_weight:" + str(counts_loss_weight))
+
+    # read in arguments
+    seed = args.seed
+    np.random.seed(seed)
     tf.random.set_seed(seed)
     rn.seed(seed)
 
-    #define inputs
-    inp = Input(shape=(sequence_len, 4),name='sequence')    
+    # define inputs
+    inp = Input(shape=(sequence_len, 4), name='sequence')
 
     # first convolution without dilation
     x = Conv1D(filters,
-                kernel_size=conv1_kernel_size,
-                padding='valid', 
-                activation='relu',
-                name='bpnet_1st_conv')(inp)
+               kernel_size=conv1_kernel_size,
+               padding='same',
+               activation='relu',
+               name='bpnet_1st_conv')(inp)
 
-    layer_names = [str(i) for i in range(1,n_dil_layers+1)]
+    layer_names = [str(i) for i in range(1, n_dil_layers + 1)]
     for i in range(1, n_dil_layers + 1):
         # dilated convolution
-        conv_layer_name = 'bpnet_{}conv'.format(layer_names[i-1])
-        conv_x = Conv1D(filters, 
-                        kernel_size=3, 
-                        padding='valid',
-                        activation='relu', 
-                        dilation_rate=2**i,
+        conv_layer_name = 'bpnet_{}conv'.format(layer_names[i - 1])
+        conv_x = Conv1D(filters,
+                        kernel_size=3,
+                        padding='same',
+                        activation='relu',
+                        dilation_rate=2 ** i,
                         name=conv_layer_name)(x)
 
         x_len = int_shape(x)[1]
         conv_x_len = int_shape(conv_x)[1]
-        assert((x_len - conv_x_len) % 2 == 0) # Necessary for symmetric cropping
+        assert ((x_len - conv_x_len) % 2 == 0)  # Necessary for symmetric cropping
 
-        x = Cropping1D((x_len - conv_x_len) // 2, name="bpnet_{}crop".format(layer_names[i-1]))(x)
+        x = Cropping1D((x_len - conv_x_len) // 2, name="bpnet_{}crop".format(layer_names[i - 1]))(x)
         x = add([conv_x, x])
 
     # Branch 1. Profile prediction
     # Step 1.1 - 1D convolution with a very large kernel
     prof_out_precrop = Conv1D(filters=num_tasks,
-                        kernel_size=profile_kernel_size,
-                        padding='valid',
-                        name='prof_out_precrop')(x)
+                              kernel_size=profile_kernel_size,
+                              padding='same',
+                              name='prof_out_precrop')(x)
 
     # Step 1.2 - Crop to match size of the required output size
-    cropsize = int(int_shape(prof_out_precrop)[1]/2)-int(out_pred_len/2)
-    assert cropsize>=0
-    assert (int_shape(prof_out_precrop)[1] % 2 == 0) # Necessary for symmetric cropping
+    cropsize = int(int_shape(prof_out_precrop)[1] / 2) - int(out_pred_len / 2)
+    assert cropsize >= 0
+    assert (int_shape(prof_out_precrop)[1] % 2 == 0)  # Necessary for symmetric cropping
     prof = Cropping1D(cropsize,
-                name='logits_profile_predictions_preflatten')(prof_out_precrop)
+                      name='logits_profile_predictions_preflatten')(prof_out_precrop)
 
     # Branch 2. Counts prediction
     # Step 2.1 - Global average pooling along the "length", the result
@@ -83,17 +83,17 @@ def getModelGivenModelOptionsAndWeightInits(args, model_params):
 
     profile_out = Flatten(name="logits_profile_predictions")(prof)
 
-    gap_combined_conv = GlobalAvgPool1D(name='gap')(x) # acronym - gapcc
+    gap_combined_conv = GlobalAvgPool1D(name='gap')(x)  # acronym - gapcc
 
     # Step 2.3 Dense layer to predict final counts
     count_out = Dense(num_tasks, name="logcount_predictions")(gap_combined_conv)
 
     # instantiate keras Model with inputs and outputs
-    model=Model(inputs=[inp],outputs=[profile_out, count_out])
+    model = Model(inputs=[inp], outputs=[profile_out, count_out])
 
     model.compile(optimizer=Adam(learning_rate=args.learning_rate),
-                    loss=[multinomial_nll,'mse'],
-                    loss_weights=[1,counts_loss_weight])
+                  loss=[multinomial_nll, 'mse'],
+                  loss_weights=[1, counts_loss_weight])
 
     return model 
 
